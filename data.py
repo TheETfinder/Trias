9af175acb7e0aca33509c6536fcd13613b0d1a20
Etienne <TheETfinder@outlook.de>
2024-11-22 21:01:57 +0100

    not fixed yet

--- data.py
+++ data.py
@@ -5,12 +5,14 @@ import requests
 URL = "https://projekte.kvv-efa.de/mangangtrias/trias"
 
 with open('kvv.xml', 'r') as file:
-    mydata = file.read()
+    kvvdata = file.read()
 
 
 # sending get request and saving the response as response object
-r = requests.get(url = URL, data = mydata)
+r = requests.get(url = URL, data= kvvdata)
 
 print(r)
 
+
+#F76VP9vhNQa7
 

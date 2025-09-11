2c00d63242b068f9cfc53e3520a6890c594b797b
Ruby <TheETfinder@outlook.de>
2025-09-11 17:01:50 +0200

    Final push, it wrks as it should
    I got it to work :D

--- XSD/TRIAS-main/xsdtopy.py
+++
@@ -1,6 +0,0 @@
-import xmlschema
-import pprintpp
-xsd = xmlschema.XMLSchema('Trias_StopEvents.xsd')
-xml = xsd.to_dict('kvv.xml')
-
-
--- data.py
+++ data.py
@@ -9,65 +9,49 @@ URL = "https://projekte.kvv-efa.de/mangangtrias/trias"
 
 time_now = time.strftime("%Y-%m-%dT%H:%M:%S")  
 
-print(time_now)
-
 with open('kvv.xml', 'r') as file:
      mydata = file.read()
 
-data_modified = mydata.replace('2025-03-02T17:36:00',time_now)
-# sending get request and saving the response as response object
+data_modified = mydata.replace('timenow',time_now)
 
 headers = {'Content-Type': 'application/xml'} # set what your server accepts
 answer = requests.post(url= URL, data=data_modified, headers=headers).text
-#print(answer)
 
-#with open ('data.xml','w') as data:
-#    data.write(answer) 
+data = json.dumps(xmltodict.parse(answer))
 
-
-input_ = json.dumps(answer)
-
-
-inputfix1 = input_.replace("\\n","")
-inputfix2= inputfix1.replace('"', '', 1)
-inputfix3 = inputfix2.replace('>"','>', 1)
-data_trias = inputfix3.replace('\\"','"')
-
-
-trias_dict = xmltodict.parse(data_trias)
+fix = data.replace('trias:','')
 
 with open('test.json','w') as a:
-	json.dump(trias_dict, a)
+	a.write(fix)
 
-exit()
+with open('test.json','r') as l:
+	tri = json.load(l)
 
-TripResult = trias_dict["Trias"]["ServiceDelivery"]["DeliveryPayload"]["TripResponse"]["TripResult"]
+trias = tri["Trias"]
 
+#with open('trias.json','w') as d:
+#	json.dump(trias, d)
 
+#exit()
 
+StopEventResult = trias["ServiceDelivery"]["DeliveryPayload"]["StopEventResponse"]["StopEventResult"]
 
 with open('trias.json', 'w', encoding="utf-8") as trias_json:
-     json.dump(TripResult, trias_json)
+     json.dump(StopEventResult, trias_json)
 
 with open('trias.json', 'r', encoding="unicode-escape") as trias_json_load:
-    trias_data_reimport = json.load(trias_json_load)
-
-
-trip = trias_data_reimport["Trip"]["TripLeg"]
-
-with open('debug.json', 'w') as e:
-    json.dump(trip, e)
+    trip = json.load(trias_json_load)
 
     
 for i in trip :
 
-    Trip_name = i["TimedLeg"]["Service"]["PublishedLineName"]["Text"]
+    Trip_name = i["StopEvent"]["Service"]["ServiceSection"]["PublishedLineName"]["Text"]
     text_line = Trip_name.encode('latin1').decode('utf8')
 
-    Trip_time = i["TimedLeg"]["LegAlight"]["ServiceArrival"]["TimetabledTime"]
+    Trip_time = i["StopEvent"]["ThisCall"]["CallAtStop"]["ServiceDeparture"]["TimetabledTime"]
     text_time = Trip_time.encode('latin1').decode('utf8')
         
-    Trip_dest = i["TimedLeg"]["LegAlight"]["StopPointName"]["Text"]
+    Trip_dest = i["StopEvent"]["Service"]["DestinationText"]["Text"]
     text_dest = Trip_dest.encode('latin1').decode('utf8')
     
     trias_result = "Linie:" + " "+ text_line + " " +"Nach"+ " "+ text_dest + " " +"Ankunft" + " "+ text_time 
